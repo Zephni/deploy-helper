2c6967425bf9d35cca0316dece2374aebb409eb6
Craig Dennis <craig@deliverysoftware.co.uk>
2024-05-13 18:44:11 +0100

    install.php now allows overwriting of files at user request if they already exist. run.php checks to see that DeployHelper has been autoloaded. deployhelper now autoloads from ./vendor/autoload.php

--- install.php
+++ install.php
@@ -5,8 +5,8 @@ $cwd = getcwd();
 
 // Set an array of the paths and filenames we need to copy, not that the value is the destination from the cwd root
 $copyFiles = [
-    __DIR__.'/files/deployhelper.json' => 'deployhelper.json',
     __DIR__.'/files/deployhelper' => 'deployhelper',
+    __DIR__.'/files/deployhelper.json' => 'deployhelper.json',
 ];
 
 echo PHP_EOL;
@@ -19,14 +19,30 @@ foreach ($copyFiles as $source => $destination) {
         copy($source, $cwd . '/' . $destination);
 
         // With green text using ANSI escape codes, show success
-        echo "\033[0;32mFile " . $destination . ' copied successfully.' . "\033[0m" . PHP_EOL;
+        echo "\033[0;32mFile " . $destination . ' copied successfully.' . "\033[0m" . PHP_EOL . PHP_EOL;
     } else {
         // Output that the file already exists with yellow warning text, tell the user to delete it and try again
-        echo "\033[0;33mFile " . $destination . ' already exists, please delete it and try again.' . "\033[0m" . PHP_EOL;
+        // echo "\033[0;33mFile " . $destination . ' already exists, please delete it and try again.' . "\033[0m" . PHP_EOL;
+
+        // Now we instead ask the user if they want to overwrite the file
+        echo "\033[0;33mFile " . $destination . ' already exists, would you like to overwrite it? (y/n) [n]: ' . "\033[0m";
+
+        // Get the user input
+        $input = readline();
+
+        // If the user input is 'y' or 'yes', overwrite the file
+        if ($input === 'y' || $input === 'yes') {
+            // Copy the file
+            copy($source, $cwd . '/' . $destination);
+
+            // With green text using ANSI escape codes, show success
+            echo "\033[0;32mFile " . $destination . ' overwritten successfully.' . "\033[0m" . PHP_EOL . PHP_EOL;
+        } else {
+            // With grey text using ANSI escape codes, show that the file was not copied
+            echo "\033[0;37mFile " . $destination . ' not copied.' . "\033[0m" . PHP_EOL . PHP_EOL;
+        }
     }
 }
-
-echo PHP_EOL;
 
 // In white, tell the user how to run the deployhelper command
 echo "\033[0;37mTo run deployhelper, use the following command:" . PHP_EOL;
--- run.php
+++ run.php
@@ -1,6 +1,11 @@
 <?php
 
-require_once str_replace('\\', '/', __DIR__.'/../../autoload.php');
+// NOTE: WE MUST REQUIRE COMPOSER'S `autoload.php` BEFORE RUNNING THIS SCRIPT
+
+// Die if WebRegulate\DeployHelper\DeployHelper class does not exist, and note that user must autoload first
+if (!class_exists('WebRegulate\DeployHelper\DeployHelper')) {
+    die("\nDeployHelper run.php must be required after Composer's autoload.php.\nPlease check deployhelper script is up to date by running:\n./vendor/webregulate/deploy-helper/install.php\n\n");
+}
 
 use WebRegulate\DeployHelper\DeployHelper;
 
